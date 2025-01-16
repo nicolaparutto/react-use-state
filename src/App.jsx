9c00b22656c90fb342fd909dcdd66d95cfa6c1e4
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-16 18:10:57 +0100

    feat: add basic page structure

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,18 @@ import languages from "./assets/languages.js"
 
 function App() {
   return (
-    
+    <section>
+      <h1>Learn Web Development</h1>
+      <div className="buttons-container">
+        <button>Linguaggio 1</button>
+        <button>Linguaggio 2</button>
+        <button>Linguaggio 3</button>
+      </div>
+      <div className="response-container">
+        <h2>Nome del linguaggio</h2>
+        <p>Descrizione del linguaggio</p>
+      </div>
+    </section>
   )
 }
 
